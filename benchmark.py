9ce510f6f42cdcf6da48a931bad4cd63341b48ef
Diana Zoya Smirnova <zis8@cornell.edu>
2021-05-07 17:38:38 -0400

    edited benchmark script to read from file, then delete it

--- benchmark.py
+++ benchmark.py
@@ -8,19 +8,19 @@ class Automator:
         #     raise Exception("This script must be run with sudo permissions")
         
         #ASSUME REPO IS ON SAME LEVEL AS OLTPBENCH FOLDER
-        os.chdir("../oltpbench")
+        os.chdir("./oltpbench")
 
     def run_data(self, should_load = True):
         if should_load:
             os.system("sudo ./oltpbenchmark -b tpcc -c config/tpcc_config_postgres.xml --create=true --load=" + str(should_load))
+        print("about to run benchmark")
         os.system("sudo ./oltpbenchmark -b tpcc -c config/tpcc_config_postgres.xml --create=true --load=" + str(should_load) + " --execute=true -s 5 -o outputfile")
 
 
     def get_throughput(self):
         #CHANGE PATH BASED ON WHERE YOU WANT TO USE IT
         #TODO AUTOMATE THIS
-        script_dir = os.path.dirname("/results")
-        file_path = os.path.join(script_dir, 'outputfile.6.res')
+        file_path = os.path.join("./results", 'outputfile.res')
         with open(file_path, "r") as f:
             all_lines = f.readlines()
             res = all_lines[-1].split(",")[1]
@@ -33,7 +33,7 @@ class Automator:
             #     res = line.split(",")
             #     print(res[1])
             f.close()
-
+        os.system(f"sudo rm {file_path}")
 a = Automator()
-a.run_data(False)
+a.run_data(True)
 a.get_throughput()
