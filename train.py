ab9f8dc6c1022372f6e49a3d221918d1de234f22
Zoya Smirnova <zis8@cornell.edu>
2021-05-12 15:16:22 -0400

    moved train.py

--- train.py
+++ train.py
@@ -1,3 +1,65 @@
-import gym
+import numpy as np
+from datetime import datetime
 
-env = gym.make('gym_DB:postgres')
+import gym
+from gym.envs.registration import register
+from stable_baselines3 import A2C, DQN, DDPG, PPO, SAC, TD3
+from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.env_util import make_vec_env
+
+from parameter import Parameter
+
+# register envs
+register(
+    id='SimpleDiscrete-v0',
+    entry_point='envs.test_envs:SimpleDiscreteEnv'
+) 
+register(
+    id='SimpleContinuous-v0',
+    entry_point='envs.test_envs:SimpleContinuousEnv'
+) 
+register(
+    id='Postgres-v0',
+    entry_point='envs.postgres_env:PostgresEnv'
+)
+
+# initialize parameters
+random_page_cost = Parameter("random_page_cost", 1, 4, 4, 4)
+io_concurrency = Parameter("effective_io_concurrency", 1, 1000, 1, 1)
+parameters = [random_page_cost, io_concurrency]
+
+# create the env with the parameters
+env = make_vec_env('Postgres-v0', env_kwargs=parameters)
+check_env(env)
+
+# initialize model
+# model = DDPG("MlpPolicy", env, verbose=1)
+# model = DQN("MlpPolicy", env, verbose=1)
+model = PPO("MlpPolicy", env, verbose=1)
+
+# learn
+start = datetime.now()
+model.learn(total_timesteps=10)
+end = datetime.now()
+print(f'training time: {end-start}')
+
+obs = env.reset()
+actions = []
+states = []
+all_rewards = []
+
+# run the trained model
+for i in range(10):
+  # print(f'step {i}')
+  action, _states = model.predict(obs)
+  obs, rewards, done, info = env.step(action)
+  env.render()
+
+  actions.append(round(action[0],2))
+  states.append(round(env.state[0],2))
+  all_rewards.append(rewards)
+
+print(actions)
+print(states)
+# print(all_rewards)
+print(np.mean(all_rewards))
