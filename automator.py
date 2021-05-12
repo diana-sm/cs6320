af14b1c536c482d4c7113ea06046d1d9d0c7d16a
April <yy459@cornell.edu>
2021-05-11 21:53:16 -0400

    finish automator script

--- automator.py
+++ automator.py
@@ -16,40 +16,32 @@ a.reinit_database()
 print("Reinit time:", time.time() - start_time)
 #========
 
-params = dict()
+param_dict = {
+    "effective_cache_size": ['"4 GB"', '"8 GB"', '"16 GB"'],
+    "work_mem": ['"64 kB"', '"128 kB"', '"256 kB"', '"512 kB"', '"1 MB"', '"2 MB"', '"4 MB"', '"8 MB"', '"16 MB"', '"1 GB"', '"2 GB"', '"4 GB"', '"8 GB"', '"16 GB"'],
+    "maintenance_work_mem": ['"1 MB"', '"2 MB"', '"4 MB"', '"8 MB"', '"16 MB"'],
+    "effective_io_concurrency": ['"1"', '"10"', '"100"', '"500"', '"1000"'],
+    "default_statistics_target": ['"1"', '"10"', '"100"', '"500"', '"1000"', '"5000"', '"10000"'],
+    "random_page_cost": ['"1.0"', '"2.0"', "3.0"', '"4.0"'],
+    "autovacuum_scale_factor": ['"0"', '"0.5"', '"1"'],
+    "autovacuum_vacuum_threshold": ['"0"', '"1"', '"10"', '"100"', '"500"', '"1000"', '"5000"', '"10000"'],
+    "vacuum_cost_limit": ['"0 ms"', '"1 ms"', '"10 ms"', '"100 ms"', '"500 ms"', '"1000 ms"'],
+    "vacuum_cost_delay": ['"0 ms"', '"1 ms"', '"10 ms"', '"100 ms"', '"500 ms"', '"1000 ms"']
+}
 
-params = {"effective_cache_size", "work_mem", "maintenance_work_mem",
-"effective_io_concurrency", "default_statistics_target", "random_page_cost",
-"autovacuum_scale_factor", "autovacuum_vacuum_threshold", "vacuum_cost_limit",
-"vacuum_cost_delay"}
+for param, metrics in param_dict.items():
+    start_time = time.time()
+    for m in metrics:
+        p.param_set(param, m)
 
-for i in params:
-  
-
-
-start_time = time.time()
-
-for p in param_lst:
-  for i in ['"128 kB"', '"256 kB"', '"512 kB"', '"1 GB"', '"2 GB"', '"4 GB"', '"8 GB"']:
-      #TODO: decide on better intervals (I just went with powers of two)
-
-      
-      p.param_set(p, i)
-      p.restart()
-
-#NOTE: for some reason kilobytes needs a lowercase "k"-- be careful about this one
-for i in ['"128 kB"', '"256 kB"', '"512 kB"', '"1 GB"', '"2 GB"', '"4 GB"', '"8 GB"']:
-    #TODO: decide on better intervals (I just went with powers of two)
-
-    
-    p.param_set("shared_buffers", i)
+        a.run_data()
+        print(a.get_throughput())
+    # restart after every set of param has fully iterated through its list of metrics
+    # reinit after every param is done going through its metrics
     p.restart()
+    a.reinit_database()
+    print("Reinit time:", time.time() - start_time)
 
-    #can use this line to verify that params were changed successfully
-    #os.system("sudo -u postgres psql -c 'SHOW effective_io_concurrency'")
-
-    a.run_data()
-    print(a.get_throughput())
 print("Benchmark time:", time.time() - start_time)
-# reset after done, just in case
+
 p.reset()
